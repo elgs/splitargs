f339635e6191fa7f4d5c531c6a46fecebc02c89a
elgs <elgs1980@hotmail.com>
2014-07-02 04:12:25 +0800

    All tests passed.

--- splitargs.js
+++ splitargs.js
@@ -6,15 +6,54 @@
     "use strict";
 
     module.exports = function (input, separator) {
+        separator = separator || /\s/g;
+        var singleQuoteOpen = false;
+        var doubleQuoteOpen = false;
+        var tokenBuffer = [];
+        var ret = [];
 
-        var config = {
-            singleQuoteOpen: false,
-            doubleQuoteOpen: false,
-            separator: separator || /\s+/g
-        };
+        var arr = input.split('');
+        for (var i = 0; i < arr.length; ++i) {
+            var element = arr[i];
+            var matches = element.match(separator);
+            if (element === "'") {
+                if (!doubleQuoteOpen) {
+                    singleQuoteOpen = !singleQuoteOpen;
+//                    if (!singleQuoteOpen) {
+//                        ret.push(tokenBuffer.join(''));
+//                        tokenBuffer = [];
+//                    }
+                    continue;
+                }
+            } else if (element === '"') {
+                if (!singleQuoteOpen) {
+                    doubleQuoteOpen = !doubleQuoteOpen;
+//                    if (!doubleQuoteOpen) {
+//                        ret.push(tokenBuffer.join(''));
+//                        tokenBuffer = [];
+//                    }
+                    continue;
+                }
+            }
 
-        input.split('').map(function (element) {
-
-        }, config);
+            if (!singleQuoteOpen && !doubleQuoteOpen) {
+                if (matches) {
+                    if (tokenBuffer && tokenBuffer.length > 0) {
+                        ret.push(tokenBuffer.join(''));
+                        tokenBuffer = [];
+                    }
+                } else {
+                    tokenBuffer.push(element);
+                }
+            } else if (singleQuoteOpen) {
+                tokenBuffer.push(element);
+            } else if (doubleQuoteOpen) {
+                tokenBuffer.push(element);
+            }
+        }
+        if (tokenBuffer && tokenBuffer.length > 0) {
+            ret.push(tokenBuffer.join(''));
+        }
+        return ret;
     };
 })();

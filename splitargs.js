66d5e0ea24b219aa68b322e7151be60296fe6ab7
elgs <elgs1980@hotmail.com>
2015-10-03 23:04:45 -0700

    Added keepQuotes option.

--- splitargs.js
+++ splitargs.js
@@ -5,7 +5,7 @@
 (function () {
     "use strict";
 
-    module.exports = function (input, separator) {
+    module.exports = function (input, separator, keepQuotes) {
         separator = separator || /\s/g;
         var singleQuoteOpen = false;
         var doubleQuoteOpen = false;
@@ -16,30 +16,26 @@
         for (var i = 0; i < arr.length; ++i) {
             var element = arr[i];
             var matches = element.match(separator);
-            if (element === "'") {
-                if (!doubleQuoteOpen) {
-                    singleQuoteOpen = !singleQuoteOpen;
-                    continue;
-                }
-            } else if (element === '"') {
-                if (!singleQuoteOpen) {
-                    doubleQuoteOpen = !doubleQuoteOpen;
-                    continue;
-                }
-            }
-
-            if (!singleQuoteOpen && !doubleQuoteOpen) {
-                if (matches) {
-                    if (tokenBuffer && tokenBuffer.length > 0) {
-                        ret.push(tokenBuffer.join(''));
-                        tokenBuffer = [];
-                    }
-                } else {
+            if (element === "'" && !doubleQuoteOpen) {
+                if (keepQuotes === true) {
                     tokenBuffer.push(element);
                 }
-            } else if (singleQuoteOpen) {
-                tokenBuffer.push(element);
-            } else if (doubleQuoteOpen) {
+                singleQuoteOpen = !singleQuoteOpen;
+                continue;
+            } else if (element === '"' && !singleQuoteOpen) {
+                if (keepQuotes === true) {
+                    tokenBuffer.push(element);
+                }
+                doubleQuoteOpen = !doubleQuoteOpen;
+                continue;
+            }
+
+            if (!singleQuoteOpen && !doubleQuoteOpen && matches) {
+                if (tokenBuffer && tokenBuffer.length > 0) {
+                    ret.push(tokenBuffer.join(''));
+                    tokenBuffer = [];
+                }
+            } else {
                 tokenBuffer.push(element);
             }
         }
